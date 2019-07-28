7000096e562d40d220d0d9299578cce4676e4fed
alexandrapricop <49493413+alexandrapricop@users.noreply.github.com>
2019-07-28 04:15:44 +0300

    Something new

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// VARIABLES
 var menu = document.querySelector('.menu');
 var drop = document.querySelector('#list');
 var upperMenu = document.querySelector('.mobile');
 var buttonAbout = document.querySelector('.mobile-hidden');
-
+var showcase = document.querySelector('.showcase');
+var wH = window.innerHeight;
+// CALLBACK FUNCTIONS
 function classToggle(){
     drop.classList.toggle('hidden');
     drop.classList.toggle('links');
@@ -14,5 +17,12 @@ function moveToAbout(){
     window.scrollTo(100);
 }
 
+// EVENT LISTENERS
 menu.addEventListener('click', classToggle);
 buttonAbout.addEventListener('click', moveToAbout);
+
+// OTHERS
+showcase.style.height = wH;
+console.log(showcase.style.height);
+
+
